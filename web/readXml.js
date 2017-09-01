49277a22dc05b70df27a36a9c13aac6f8746b578
Elliot Pak <pakelli998@gmail.com>
2017-09-01 11:42:28 +0800

    starting to make the tagging web app

---
+++ web/readXml.js
@@ -0,0 +1,28 @@
+xmlDoc = null;
+
+function loadFile()
+{
+    //var myFile = $("#uploadFile")
+    input = document.getElementById("uploadFile");
+    if (!input){
+        alert("1");
+    }
+    else if (!input.files)
+    {
+        alert("2");
+    }
+    else if (!input.files[0])
+    {
+        alert("3");
+    }
+    else
+    {
+        file = input.files[0];
+        fr = new FileReader();
+        fr.onload = function () {
+            xmlText = fr.result;
+            xmlDoc = new DOMParser().parseFromString(xmlText, "text/xml")
+        };
+        fr.readAsText(file);
+    }
+}
