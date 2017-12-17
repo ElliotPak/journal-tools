7b29f20dc821a79ec5397e2692a4b3ae490f777d
Elliot Pak <pakelli998@gmail.com>
2017-12-17 12:59:51 +0800

    now displays if there arent quotes/tags/notes

--- web/search.js
+++ web/search.js
@@ -37,38 +37,71 @@ function getTimeElements(node, type)
     return elements;
 }
 
-function displayFile(file)
+function getNoteContents(file)
 {
-    halfTag = $('<div class="displayHalf"></div>');
-    halfTag.append('<span class="displaySubheader">Tags:</span><br>');
-    tagList = $('<ul class="tagList"></ul>');
-    halfTag.append(tagList);
-    tags = getTimeElements(file, "Tag");
-    tags.forEach( function(entry) {
-        tagEntry = $("<li>" + entry + "</li>")
-        tagList.append(tagEntry);
-    });
-
-    halfQuote = $('<div class="displayHalf"></div>');
-    halfQuote.append('<span class="displaySubheader">Quotes:</span><br>');
-    quoteList = $('<ul class="quoteList"></ul>');
-    halfQuote.append(quoteList);
-    quotes = getTimeElements(file, "Quote");
-    quotes.forEach( function(entry) {
-        quoteEntry = $("<li>" + entry + "</li>")
-        quoteList.append(quoteEntry);
-    });
-
-    displayNode = $('<div class="displayFile"></div>');
-    displayNode.append('<span class="displayFilename">' + $(file).attr("name") + '</span><br>');
-    displayNode.append(halfTag);
-    displayNode.append(halfQuote);
+    toReturn = "";
     noteSelector = $(file).find("Notes");
     if (noteSelector.length > 0)
     {
+        node = noteSelector[0];
+        console.log(noteSelector[0]);
+        toReturn = node.innerHTML;
+        toReturn = toReturn.replace(/\n/g, "<br />");
+    }
+    return toReturn;
+}
+
+function displayFile(file)
+{
+    halfTag = $('<div class="displayHalf"></div>');
+    tags = getTimeElements(file, "Tag");
+    if (tags.length > 0)
+    {
+        halfTag.append('<span class="displaySubheader">Tags:</span><br>');
+        tagList = $('<ul class="tagList"></ul>');
+        halfTag.append(tagList);
+        tags.forEach( function(entry) {
+            tagEntry = $("<li>" + entry + "</li>")
+            tagList.append(tagEntry);
+        });
+    }
+    else
+    {
+        halfTag.append('<span class="displaySubheader">No tags.</span><br>');
+    }
+
+    halfQuote = $('<div class="displayHalf"></div>');
+    quotes = getTimeElements(file, "Quote");
+    if (quotes.length > 0)
+    {
+        halfQuote.append('<span class="displaySubheader">Quotes:</span><br>');
+        quoteList = $('<ul class="quoteList"></ul>');
+        halfQuote.append(quoteList);
+        quotes.forEach( function(entry) {
+            quoteEntry = $("<li>" + entry + "</li>")
+            quoteList.append(quoteEntry);
+        });
+    }
+    else
+    {
+        halfQuote.append('<span class="displaySubheader">No quotes.</span><br>');
+    }
+
+    displayNode = $('<div class="displayFile"></div>');
+    displayNode.append('<span class="displayFilename">' + $(file).attr("name") + '</span><br>');
+    halfContainer = $('<div class="halfContainer"></div>');
+    halfContainer.append(halfTag);
+    halfContainer.append(halfQuote);
+    displayNode.append(halfContainer);
+    noteNode = getNoteContents(file);
+    if (noteNode !== "")
+    {
         displayNode.append('<br><span class="displaySubheader">Notes:</span><br>');
-        console.log(noteSelector)
-        displayNode.append('<span class="displayText">' + noteSelector[0].innerHTML + '</span>');
+        displayNode.append('<span class="displayText">' + noteNode + '</span>');
+    }
+    else
+    {
+        displayNode.append('<br><span class="displaySubheader">No notes.</span>')
     }
     $("#results").append(displayNode);
     $("#results").append("<br>");
